a6d1bc054212fbfd41b3b79409983832b0aebf78
staticsafe <sadiq.9541@gmail.com>
2011-12-15 07:53:59 -0500

    script now only runs as root

--- webserversetup.sh
+++ webserversetup.sh
@@ -12,12 +12,16 @@ printf '%s\n' "$@" >&2
 exit 1
 }
 
+#Root check
+if  [[ "$(/usr/bin/whoami)" != "root" ]]; then
+	printf '%s\n' 'Run this script as root or using sudo if you have it!'
+	die 'exiting'
+fi
+
 #Update repos and check for any update(s)
 
-sudo apt-get update
-sudo apt-get upgrade
-
-
+apt-get update
+apt-get upgrade
 
 #Install webserver packages
 #Needs to be more user-friendly, add links to docs etc.
@@ -28,30 +32,30 @@ printf '%s\n' 'Choose a webserver to install, your choices are Apache, Lightttpd
 read webserver
 if [[ "$webserver" ==  "Apache" || "$webserver" == "apache" ]]; then
 	printf '%s\n' 'Installing LAMP packages now'
-	sudo apt-get install mysql-server mysql-client apache2 apache2-doc php5 php5-mysql libapache2-mod-php5
+	apt-get install mysql-server mysql-client apache2 apache2-doc php5 php5-mysql libapache2-mod-php5
 	printf '%s\n' 'Stopping started LAMP services so you can configure them properly.'
-	sudo /etc/init.d/mysql stop
-	sudo /etc/init.d/apache2 stop
+	/etc/init.d/mysql stop
+	/etc/init.d/apache2 stop
 	printf '%s\n' 'Done! :)'
 elif [["$webserver" == "nginx" || "$webserver" == "Nginx" ]]; then
 	printf '%s\n' 'Installing nginx based webserver now'
-	sudo apt-get install nginx php5-cli php5-cgi spawn-fcgi mysql-server mysql-client php5-mysql
+	apt-get install nginx php5-cli php5-cgi spawn-fcgi mysql-server mysql-client php5-mysql
 	printf '%s\n' 'Stopping started services so you can configure them properly.'
-	sudo /etc/init.d/mysql stop
-	sudo /etc/init.d/nginx stop
+	/etc/init.d/mysql stop
+	/etc/init.d/nginx stop
 	printf '%s\n' 'Look at http://goo.gl/tQBe7 for instructions on how to configure php-fastcgi'
 	printf '%s\n' 'Done! :)'
 elif [["$webserver" == "Lighttpd" || "$webserver" == "lighttpd" ]]; then
 	printf '%s\n' 'Installing lighttpd based webserver now'
-	sudo apt-get install lighttpd mysql-server mysql-client php5-mysql
+	apt-get install lighttpd mysql-server mysql-client php5-mysql
 	printf '%s\n' 'Stopping started services so you can configure them properly.'
-	sudo /etc/init.d/mysql stop
-	sudo /etc/init.d/nginx stop
+	/etc/init.d/mysql stop
+	/etc/init.d/nginx stop
 	printf '%s\n' 'Look at http://goo.gl/i4NlE for instructions on how to configure lighttpd.'
 	printf '%s\n' 'Done! :)'
 elif [["$webserver" == "cherokee" || "$webserver" == "Cherokee" ]]; then
 	printf '%s\n' 'Installing Cherokee based webserver now'
-	sudo apt-get install cherokee mysql-server mysql-client php5-mysql openssl
+	apt-get install cherokee mysql-server mysql-client php5-mysql openssl
 	printf '%s\n' 'Done! :)'
 else
 	installwebserver
@@ -77,7 +81,7 @@ elif [["$cpinput" == "Yes" || "$cpinput" == "Yes"]]; then
 		printf '%s\n' 'Alright, not installing phpmyAdmin.'
 	elif [["$cppma" == "Yes" || "$cppma" == "yes"]]; then
 		printf '%s\n' 'Installing phpmyAdmin!'
-		sudo apt-get install phpmyadmin
+		apt-get install phpmyadmin
 	else
 		installcp
 	fi
@@ -90,18 +94,11 @@ elif [["$cpinput" == "Yes" || "$cpinput" == "Yes"]]; then
 	elif [["$cpwebmin" == "Yes" || "$cpwebmin" == "yes"]]; then
 		printf '%s\n' 'Installing Webmin!'
 		wget http://www.webmin.com/jcameron-key.asc
-		sudo apt-key add jcameron-key.asc
+		apt-key add jcameron-key.asc
 		printf '%s\n' 'Adding Webmin APT repo to sources.list'
-		cp /etc/apt/sources.list $HOME/sources.list
-		sudo chmod 777 $HOME/sources.list
 		echo 'deb http://download.webmin.com/download/repository sarge contrib' >> $HOME/sources.list
-		sudo mv $HOME/sources.list /etc/apt/sources.list
-		sudo chmod 644 /etc/apt/sources.list
-		sudo chown root:root /etc/apt/sources.list
-		sudo apt-get update
-		sudo apt-get install webmin
-		sudo -k
-		# I realize this can be done in a better way, but meh I'll improve it later.
+		apt-get update
+		apt-get install webmin
 	else
 		installcp
 	fi
