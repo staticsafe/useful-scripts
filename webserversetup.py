3231a4b11254e6064911a815c58711a65d86ad4d
staticsafe <sadiq.9541@gmail.com>
2011-12-17 17:32:35 -0500

    finished debian part..mostly

--- webserversetup.py
+++ webserversetup.py
@@ -3,6 +3,7 @@
 import os
 from subprocess import call
 import sys
+import platform
 
 
 def main():
@@ -12,22 +13,48 @@ if uid != 0:
 	print "This script must be run as root or sudo if you have it!"
 	sys.exit()
 #distrocheck
-if os.path.isfile(/etc/fedora-release) = True:
+userdistro = platform.linux_distribution()
+
+if userdistro[0] == "Fedora":
 	webserverfedora()
 	installcpfedora()
-elif os.path.isfile(/etc/debian_version) = True:
+elif userdistro[0] == "debian":
 	webserverdebian()
-	installcpfedora()
+	installcpdebian()
 elif os.path.isfile(/etc/arch-release) = True:
 	webserverarch()
 	installcparch()
 else:
-	print "This is script is not supported for your OS, exiting."
+	print "This is script is not supported for your distro, exiting."
 	sys.exit()
 
 
 
 def webserverdebian():
+print "Choose a webserver to install, your choices are Apache, Lightttpd, nginx, or Cherokee"
+prompt = ">"
+answer = raw_input(prompt)
+if answer == "Apache" or answer == "apache":
+	installprocessapache = call("apt-get install --assume-yes mysql-server mysql-client apache2 apache2-doc php5 php5-mysql libapache2-mod-php5")
+	print "Stopping started LAMP services so you can configure them properly."
+	stopservicesapache = call ("/etc/init.d/apache stop && /etc/init.d/mysql stop")
+elif answer == "Lighttpd" or answer == "lighttpd":
+	installprocesslighttpd = call ("apt-get install --assume-yes lighttpd mysql-server mysql-client php5-mysql")
+	print "Stopping started LAMP services so you can configure them properly."
+	stopserviceslighttpd = call ("/etc/init.d/lighttpd stop && /etc/init.d/mysql stop")
+	print "Look at http://goo.gl/i4NlE for instructions on how to configure lighttpd."
+elif answer == "nginx" or answer == "Nginx":
+	installprocessnginx = call ("apt-get install --assume-yes nginx php5-cli php5-cgi spawn-fcgi mysql-server mysql-client php5-mysql")
+	print "Stopping services so you can configure them properly"
+	stopservicesnginx = call ("/etc/init.d/nginx stop && /etc/init.d/mysql stop")
+	print "Look at http://goo.gl/tQBe7 for instructions on how to configure php-fastcgi"
+elif answer == "Cherokee" or answer == "cherokee":
+	installprocesscherokee = call ("apt-get install --assume-yes cherokee mysql-server mysql-client php5-mysql openssl")
+	print "Stopping services so you can configure them properly"
+	stopservicescherokee = call ("/etc/init.d/cherokee stop && /etc/init.d/mysql stop")
+else:
+	print "Input is invalid, please retry!"
+	webserverdebian()
 
 
 
