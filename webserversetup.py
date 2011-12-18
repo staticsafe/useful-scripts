d6cac9ce3e5e4e7c2033e0778d9588a60310f1ee
staticsafe <sadiq.9541@gmail.com>
2011-12-17 22:36:41 -0500

    eh Arch

--- webserversetup.py
+++ webserversetup.py
@@ -207,7 +207,7 @@ def main():
 	elif userdistro[0] == "debian":
 		webserverdebian()
 		installcpdebian()
-	elif userdistro[0] == "Arch":
+	elif userdistro[0] == "Arch" or os.path.isfile(/etc/arch-release) = True:
 		webserverarch()
 		installcparch()
 	elif userdistro[0] == "Ubuntu":
