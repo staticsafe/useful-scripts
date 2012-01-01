6990e82827586a14fdd027dc1babc8b8da5a1ad9
staticsafe <sadiq.9541@gmail.com>
2012-01-01 18:04:57 -0500

    maybe now?

--- webserversetup.py
+++ webserversetup.py
@@ -19,7 +19,8 @@ def webserverdebian():
 	print 'Choose a webserver to install, your choices are Apache, Lightttpd, nginx, or Cherokee'
 	prompt = ">"
 	answer = raw_input(prompt)
-	if answer == re.match(I, 'apache'):
+	regex = re.compile(answer, re.I)
+	if answer == regex.match("apache"):
 		installprocessapache = call("apt-get install --assume-yes mysql-server mysql-client apache2 apache2-doc php5 php5-mysql libapache2-mod-php5", shell=True)
 		print "Stopping started LAMP services so you can configure them properly."
 		stopservicesapache = call ("/etc/init.d/apache2 stop && /etc/init.d/mysql stop", shell=True)
