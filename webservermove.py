b01b41a4a928f4d2f342feffca7b2774629dcfd1
staticsafe <sadiq.9541@gmail.com>
2012-02-13 20:25:04 -0500

    starting new script

---
+++ webservermove.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# This script helps in the moving process of web servers (files, databases)
+
+import ConfigParser, os, requests
+from subprocess import call
+
+# global vars
+
+
+def getdefaultconfig():
+
+
+def filemove():
+
+
+def dbmove():
+
+
+def main():
+
+
+if __name__ == "__main__":
+	main()
